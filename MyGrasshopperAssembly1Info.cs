bdac782feaaef53a9128a46746db5841310f6fa8
Yi Huang <yhuang8@gmail.com>
2024-05-29 22:02:08 -0400

    GH component init.

---
+++ MyGrasshopperAssembly1Info.cs
@@ -0,0 +1,26 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace MyGrasshopperAssembly1
+{
+    public class MyGrasshopperAssembly1Info : GH_AssemblyInfo
+    {
+        public override string Name => "MyGrasshopperAssembly1";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("0864a065-9ac1-4913-a82a-8d03c3a13f10");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
